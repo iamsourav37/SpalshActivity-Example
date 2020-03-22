e880c364980ed1d0f47cb382b5db730bae1281e2
iamSourav37 <souravsrk120@gmail.com>
2020-03-22 22:26:28 +0530

    Using Thread class and using Runnable interface example

--- app/src/main/java/com/backpakcersourav/basiccomponent/SpalshActivity.java
+++ app/src/main/java/com/backpakcersourav/basiccomponent/SpalshActivity.java
@@ -7,37 +7,40 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class SpalshActivity extends AppCompatActivity {
 
-    private static final int TIME_DELAY = 10;
+    private static final int TIME_DELAY = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_spalsh);
+
         getSupportActionBar().hide();
+        setContentView(R.layout.activity_spalsh);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SpalshActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },1000 * TIME_DELAY);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(SpalshActivity.this,MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        },1000 * TIME_DELAY);
 
-//        LofoLauncher lofoLauncher = new LofoLauncher();
-//        lofoLauncher.start();
+        SpalshScreenLauncher spalshScreenLauncher = new SpalshScreenLauncher();
+        spalshScreenLauncher.start();
 
 
 
 
 
     }
-    private class LofoLauncher extends Thread{
+    private class SpalshScreenLauncher extends Thread{
         @Override
         public void run() {
             super.run();
